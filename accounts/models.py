fd61ff44343277175ec73c1aff4f3a8ee9b628b9
Jubril Issa <issa.jubril@andela.com>
2018-09-13 10:42:14 +0100

    Create the account model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import  AbstractBaseUser
 
-# Create your models here.
+from accounts.managers.user_manager import UserManager
+from utils.constants import STUDENT, TEACHER
+
+
+class User(AbstractBaseUser):
+    
+    USER_TYPE_CHOICES = (
+        (STUDENT, 'Student'),
+        (TEACHER, 'Teacher'),
+    )
+
+    user_type = models.CharField(max_length=20, choices=USER_TYPE_CHOICES, default=STUDENT)
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+    date_joined = models.DateTimeField(auto_now_add=True)
+    email = models.EmailField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = [] # Email & Password are required by default.
+
+    def get_full_name(self):
+        # The user is identified by their email address
+        return self.email
+
+    def get_short_name(self):
+        # The user is identified by their email address
+        return self.email
+
+    def __str__(self):    
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
---
+++ utils/constants.py
@@ -0,0 +1,3 @@
+# ROLES
+STUDENT = 'student'
+TEACHER = 'teacher'
