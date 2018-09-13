12e18e7f01e361af6fa10d74e347a899a831fdb8
Jubril Issa <issa.jubril@andela.com>
2018-09-13 10:43:46 +0100

    Create the student and teacher model

--- students/models.py
+++ students/models.py
@@ -1,3 +1,23 @@
+from django.conf import settings
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from utils.constants import STUDENT
 
-# Create your models here.
+
+class Student(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.email
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created and instance.user_type == STUDENT:
+        Student.objects.create(user=instance)
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def save_user_profile(sender, instance, **kwargs):
+    if instance.user_type == STUDENT:
+        instance.student.save()
--- teachers/models.py
+++ teachers/models.py
@@ -1,3 +1,21 @@
+from django.conf import settings
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from utils.constants import TEACHER
 
-# Create your models here.
+
+
+class Teacher(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created and instance.user_type == TEACHER:
+        Teacher.objects.create(user=instance)
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def save_user_profile(sender, instance, **kwargs):
+    if instance.user_type == TEACHER:
+        instance.teacher.save()
